1330635912bf45a2c37a394e4c8411b9643df3e5
Kevin Davis <45161204+kevindavis-gd@users.noreply.github.com>
2019-01-27 18:03:32 -0600

    Update xyPoint.h

--- xyPoint.h
+++ xyPoint.h
@@ -1,13 +1,3 @@
-/*
-Name:Kevin Davis
-Course:CS1063, Fall 2018, Dr.Johnson
-Purpose: This program creates points with  random x and y values
-and stores them in an array.
-It prints those values then finds the distance between
-the consecutive points and prints them out.
-It then sorts the points and repeats the process.
-*/
-
 #pragma once
 class xyPoint
 {
